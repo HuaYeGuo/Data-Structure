e1563f55ceb27f008d40119894357e349e0d4889
wq//by HUA YE GUO <125035762+HuaYeGuo@users.noreply.github.com>
2023-10-11 19:04:52 +0800

    Create 二分查找.java

---
+++ 基础算法/二分查找.java
@@ -0,0 +1,19 @@
+public class Binary_Search{
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 6, 7, 8, 10, 11, 13, 15};
+        System.out.println(Search(arr,11));
+    }
+    public static int Search(int[]arr,int target) {
+        int left = 0,right = arr.length - 1;
+        while (left <= right){
+            int mid = (left + right) <<< 1;
+            if (arr[mid] < target)
+                left = mid + 1;
+            else if(arr[mid] > target)
+                right = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
